b7a12f7bed6bd291f04cf1a077339b21c11307ad
Haneen <haneencs@hotmail.com>
2023-03-17 19:29:31 +0300

    -

--- webform.php
+++ webform.php
@@ -1,185 +1,48 @@
-<?php
-$message_sent = false;
-if(isset($_POST['name']) && ($_POST['name']) !=''){
+<?php $errors = '';
+$myemail = 'halhomoz1@hotmail.com@website.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
 
-    if(filter_var($_POST['name'], FILTER_VALIDATE_EMAIL)){
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
 
-        $userName = $_POST['name'];
-        $userEmail = $_POST['email'];
-        $messageSubject = $_POST['subject'];
-        $message = $_POST['message'];
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
 
-        $to = "halhomoz1@hotmail.com";
 
-        $body = "";
+if( empty($errors))
 
-        $body .= "From: " .$userName. "\r\n";
-        $body .= "Email: " .$userEmail. "\r\n";
-        $body .= "Message: " .$message. "\r\n";
+{
 
-        mail($to,$messageSubject,$body);
+$to = $myemail;
 
-        $message_sent = true;
+$email_subject = "Contact form submission: $name";
 
-    }
-    else{
-        $invalid_class_name = "form_invalid";
-    }
+$email_body = "You have received a new message. ".
+
+" Here are the details:\n Name: $name \n ".
+
+"Email: $email_address\n Message \n $message";
+
+$headers = "From: $myemail\n";
+
+$headers .= "Reply-To: $email_address";
+
+mail($to,$email_subject,$email_body,$headers);
+
+//redirect to the 'thank you' page
+
+header('Location: contact-form-thank-you.html');
 
 }
 
 ?>
-
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="webform.css" media="all">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>    
-    <script src="main.js"></script>
-
-    <style>:root{
-    --font-color:#555;
-    --font-hover-color:orange;
-}
-body{
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    height:100vh;
-    font-family:"Raleway", sans-serif;
-    background-color:#627b8a;
-}
-
-.container{
-    width:500px;
-    box-shadow: 0 15px 35px rgba(50,50,93,.1),0 5px 15px rgba(0,0,0,.07);
-    padding:2em;
-    background-color:#fff;
-}
-.form-group{
-    margin-bottom:1.5em;
-    transition:all .3s;
-}
-.form-label{
-    font-size:.75em;
-    color:var(--font-color);
-    display:block;
-    opacity:0;
-    transition: all .3s;
-    transform:translateX(-50px);
-}
-.form-control{
-    box-shadow:none;
-    border-radius:0;
-    border-color:#ccc;
-    border-style:none none solid none;
-    width:100%;
-    font-size:1.25em;
-    transition:all .6s;
-}
-.form-control::placeholder{
-    color:#aaa;
-}
-.form-control:focus{
-    box-shadow:none;
-    border-color:var(--font-hover-color);
-    outline:none;
-}
-.form-group:focus-within{
-    transform:scale(1.1,1.1);
-}
-
-.form-control:invalid:focus{
-    border-color:red;
-}
-.form-control:valid:focus{
-    border-color:green;
-}
-
-.btn{
-    background: 0 0 #fff;
-    border:1px solid #aaa;
-    border-radius:3px;
-    color:var(--font-color);
-    font-size:1em;
-    padding:10 50px;
-    text-transform:uppercase;
-}
-.btn:hover{
-    border-color:var(--font-hover-color);
-    color:var(--font-hover-color);
-}
-textarea{
-    resize:none;
-}
-
-
-
-.focused > .form-label{
-    opacity:1;
-    transform:translateX(0px);
-
-}</style>
-</head>
-
-<body>
-    <?php
-    if($message_sent):
-    ?>
-
-        <h3>Thanks, we'll be in touch</h3>
-
-    <?php
-    else:
-    ?>
-
-
-    <div class="container">
-        <form action="webform.php" method="POST" class="form">
-            <div class="form-group">
-                <label for="name" class="form-label">Your Name</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Jane Doe" tabindex="1" required>
-            </div>
-            <div class="form-group">
-                <label for="email" class="form-label">Your Email</label>
-                <input type="email" class="form-control <?php $invalid_class_name ?? "" ?>" id="email" name="email" placeholder="jane@doe.com" tabindex="2" required>
-            </div>
-            <div class="form-group">
-                <label for="subject" class="form-label">Subject</label>
-                <input type="text" class="form-control" id="subject" name="subject" placeholder="Hello There!" tabindex="3" required>
-            </div>
-            <div class="form-group">
-                <label for="message" class="form-label">Message</label>
-                <textarea class="form-control" rows="5" cols="50" id="message" name="message" placeholder="Enter Message..." tabindex="4"></textarea>
-            </div>
-            <div>
-                <button type="submit" class="btn">Send Message!</button>
-            </div>
-        </form>
-    </div>
-
-    <?php
-    endif;
-    ?>
-
-<script>
-    $(function() {
-   
-   $(".form-control").on('focus', function(){
-
-       $(this).parents(".form-group").addClass('focused');
-
-   });
-
-   $(".form-control").on('blur', function(){
-
-       $(this).parents(".form-group").removeClass('focused');
-
-   });
-
-});
-</script>
-</body>
-</html>
